8477edb6198f7f302093ed5ed3721832e80eba50
Allie Robinson <allierob93@gmail.com>
2025-04-08 13:38:36 -0700

    :art: Reorg code, add bootstrap

--- main.go
+++ main.go
@@ -2,28 +2,10 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8080"
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	RenderTemplate(w, "home.page.tmpl")
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	RenderTemplate(w, "about.page.tmpl")
-}
-
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("Error parsing template:", err)
-		return
-	}
-}
 
 func main() {
 	http.HandleFunc("/", Home)
